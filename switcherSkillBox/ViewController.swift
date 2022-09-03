db0ffab90fb8f4b1a8aa74a20f6bc6cdb9dc6117
Regina Mayorova <gina.rebrown@gmail.com>
2022-09-04 02:06:31 +0500

    Initial Commit

---
+++ switcherSkillBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  switcherSkillBox
+//
+//  Created by Regina Mayorova on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
