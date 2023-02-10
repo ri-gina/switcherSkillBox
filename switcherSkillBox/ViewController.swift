92d2a37a41ee90be2b58389f10a97a893697d0ed
Regina Mayorova <gina.rebrown@gmail.com>
2023-02-10 07:03:24 +0500

    commit

--- switcherSkillBox/ViewController.swift
+++ switcherSkillBox/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switcher: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeThere(_ sender: Any) {
+        if switcher.isOn {
+            self.view.backgroundColor = UIColor.white
+        }else{
+            self.view.backgroundColor = UIColor.init(
+                red: 0/255,
+                green: 0/255,
+                blue: 8/255,
+                alpha: 1.0)
+        }
+    }
 }
 
